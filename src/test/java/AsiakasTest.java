1734324e1cb2f93c71c721b4b90a8ab63606f03c
grummar <Jan.Falck@metropolia.fi>
2023-10-23 00:18:03 +0300

    testiesimerkkei lisää

--- src/test/java/AsiakasTest.java
+++ src/test/java/AsiakasTest.java
@@ -1,8 +1,19 @@
 import org.junit.jupiter.api.Test;
+import simu.model.Asiakas;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AsiakasTest {
     @Test
     void isPassport() {
+
         System.out.println("Passport test passed");
     }
+
+    @Test
+    void getCount() {
+        System.out.println("Count-test passed");
+        assertEquals(1, Asiakas.getCount());
+    }
 }
+
---
+++ src/test/java/PalvelupisteTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import simu.model.Palvelupiste;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+public class PalvelupisteTest {
+    @Test
+    void onVarattu() {
+        System.out.println("Varattutest läpi");
+    }
+}
